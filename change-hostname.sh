9556efc02369fb6815a0ad41563b7acae01b6be3
coodyme <augusto@coody.me>
2025-01-24 22:27:00 -0300

    script to change hostname

---
+++ change-hostname.sh
@@ -0,0 +1,9 @@
+HOSTNAME=$1
+
+sudo hostnamectl set-hostname $HOSTNAME
+
+sudo sed -i '' "/^127.0.0.1/ a\\
+127.0.0.1 $HOSTNAME" /etc/hosts
+sudo sh -c "echo $HOSTNAME > /etc/hostname"
+
+echo "Hostname changed to $HOSTNAME"
